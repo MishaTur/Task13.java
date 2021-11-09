be5a9e940a1a422d0d66635ff1c5ac7da7344430
MishaTur <turokmssha8888@gmail.com>
2021-11-09 12:48:15 +0200

    GOGOGO

--- src/Homewrk3/Homework50.java
+++ src/Homewrk3/Homework50.java
@@ -2,6 +2,6 @@ package Homewrk3;
 
 public class Homework50 {
     public static void main(String [ ] args){
-        System.out.println("Hello ");
+        System.out.println("Hello moi");
     }
 }
